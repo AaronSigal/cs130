fbf1d5582a8473072455fd6f9e8f7b758b707c59
Aaron <asigal55@gmail.com>
2019-10-30 18:58:30 -0700

    Added basic box functions

--- box.cpp
+++ box.cpp
@@ -12,14 +12,24 @@ bool Box::Intersection(const Ray& ray) const
 Box Box::Union(const Box& bb) const
 {
     Box box;
-    TODO;
+    box.Make_Empty();
+    box.Include_Point(bb.hi);
+	box.Include_Point(bb.lo);
+	box.Include_Point(hi);
+	box.Include_Point(lo);
     return box;
 }
 
 // Enlarge this box (if necessary) so that pt also lies inside it.
 void Box::Include_Point(const vec3& pt)
 {
-    TODO;
+    for (int i = 0; i < 3; i++) {
+    	if (pt[i] < lo[i])
+    		lo[i] = pt[i];
+
+    	if (pt[i] > hi[i])
+    		hi[i] = pt[i];
+    }
 }
 
 // Create a box to which points can be correctly added using Include_Point.
