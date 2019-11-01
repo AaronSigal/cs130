958446381aaf91eedfc1ffcf072489b0cc6e962e
Aaron <asigal55@gmail.com>
2019-10-31 23:44:26 -0700

    Lots of changes'

--- box.cpp
+++ box.cpp
@@ -2,11 +2,68 @@
 #include "box.h"
 #include "util.h"
 
+template <class T>
+void swap_element (T &a, T &b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
 // Return whether the ray intersects this box.
 bool Box::Intersection(const Ray& ray) const
 {
-    TODO;
-    return true;
+
+
+	TODO; // Remove last two checks if they aren't needed
+
+	// Declarations
+	double lowest_x, lowest_y, lowest_z;
+	double highest_x, highest_y, highest_z;
+
+	// Initial value calculation of the bounding lines
+    lowest_x = (lo[0]- ray.endpoint[0]) / ray.direction[0]; 
+	highest_x = (hi[0] - ray.endpoint[0]) / ray.direction[0]; 
+
+	lowest_y = (lo[1] - ray.endpoint[1]) / ray.direction[1]; 
+	highest_y = (hi[1] - ray.endpoint[1]) / ray.direction[1]; 
+
+	// Corrections
+	if (lowest_x > highest_x) 
+		swap_element(lowest_x, highest_x); 
+
+	if (lowest_y > highest_y) 
+		swap_element(lowest_y, highest_y); 
+
+	// Check for nonsense. If we get nonsense, that means there's no hit and we can stop (avoids doing extra work)
+	if ((lowest_x > highest_y) || (lowest_y > highest_x)) 
+		return false; 
+
+	if (lowest_y > lowest_x) 
+		lowest_x = lowest_y; 
+
+	if (highest_x > highest_y) 
+		highest_x = highest_y; 
+
+	// Initial Calculations for just Z
+	lowest_z = (lo[2] - ray.endpoint[2]) / ray.direction[2]; 
+	highest_z = (hi[2]- ray.endpoint[2]) / ray.direction[2]; 
+
+	// Correct Z
+	if (lowest_z > highest_z) 
+		swap_element(lowest_z, highest_z); 
+
+	// Check for nonsense
+	if ((lowest_x > highest_z) || (lowest_z > highest_x)) 
+		return false; 
+
+	if (lowest_z > lowest_x) 
+		lowest_x = lowest_z; 
+
+	if (highest_z < highest_x) 
+		highest_x = highest_z; 
+
+	// Return true if we never ran into nonsense
+	return true; 
 }
 
 // Compute the smallest box that contains both *this and bb.
@@ -39,3 +96,4 @@ void Box::Make_Empty()
     lo.fill(std::numeric_limits<double>::infinity());
     hi=-lo;
 }
+
--- reflective_shader.cpp
+++ reflective_shader.cpp
@@ -6,7 +6,32 @@ vec3 Reflective_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-    vec3 color;
-    TODO; // determine the color
+	vec3 color(0,0,0);
+
+	if (recursion_depth <= 0) {
+		return color;
+	}
+
+	for (unsigned int i = 0; i < world.lights.size(); i++) {
+
+		const vec3 light_to_object = world.lights[i] -> position - intersection_point;
+
+		const vec3 reflection = (-1.0 * light_to_object) + dot(light_to_object, normal) * 2 * normal; // Reflect the light_to_object vector about the normal vector
+
+		Hit reflection_hit = Closest_Intersection(reflection);
+
+		vec3 reflected_intersection_point = reflection_hit.dist * reflection.normalized();
+
+		if (reflection_hit.object != nullptr) {
+			vec3 reflection_normal = reflection_hit.object -> Normal(reflected_intersection_point, reflection_hit.part);
+		} else {
+			vec3 reflection_normal(0,0,0);
+		}
+
+		color = shader -> Shade_Surface(reflection, reflected_intersection_point, reflection_normal, recursion_depth - 1);
+
+	}
+    //vec3 color = reflectivity * reflected color + (1- reflectivity) * color;
+    //TODO; // determine the color
     return color;
 }
